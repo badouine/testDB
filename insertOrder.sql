1509e0a0816ce4406ddaae14f7cd8d54e7d93966
badouine <nzbadouine@gmail.com>
2021-11-18 13:12:38 +0000

    update insert ext sql file

---
+++ insertOrder.sql
@@ -0,0 +1,15 @@
+INSERT INTO Customer (Customer_id,Customer_name,Customer_name)
+ VALUES
+('CO1','vel',34569843),
+('CO5', 'Elhadj',7689474);
+
+INSERT INTO Orders (Customer_id,Product_id,Order_date,Quantity,Total_amount)
+ VALUES
+ ('CO2','P01',28/5/2021,3,74583),
+('CO4','P03',23/2/2021,5,57643),
+('CO5','P04',13/5/2021,0,45687);
+
+INSERT INTO Product (Prodcut_id,Prodcut_name,Category,Price)
+ VALUES
+('PO1','iPhone','Smartphone',34569843),
+('PO2','Dell','Laptop',400000);
